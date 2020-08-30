df37df5798e5a48a3d6dd057c736ddffdf15375e
Krzychu <kdogwillo@gmail.com>
2020-08-30 15:52:56 +0200

    implemented test using PO and PF

--- src/test/java/PageObjectTests.java
+++ src/test/java/PageObjectTests.java
@@ -1,9 +1,12 @@
 import PageObjects.DevToMainPage;
+import PageObjects.DevToPodcastsPage;
+import PageObjects.DevToSinglePodcastPage;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertTrue;
 public class PageObjectTests {
     WebDriver driverInPageObjectTests; //obiekt typu WebDriver
     @Before
@@ -16,5 +19,11 @@ public class PageObjectTests {
     @Test
     public void test(){
         DevToMainPage devToMainPage = new DevToMainPage(driverInPageObjectTests); // tworzymy nowy obiekt klasy DevToMainPage
+        //Przekazujemy przeglądarkę z PageObjectTests do DevToMainPage, do konstruktora
+        //Konstruktor tworzy nam nowy obiekt devToMainPage
+        DevToPodcastsPage devToPodcastsPage = devToMainPage.selectPodcasts();
+        DevToSinglePodcastPage devToSinglePodcastPage = devToPodcastsPage.selectPodcastFromList(2);
+        devToSinglePodcastPage.playPodcast();
+        assertTrue("Podcast's wasn't played",devToSinglePodcastPage.isPodcastPlaying);
     }
 }
--- src/test/java/PageObjects/DevToMainPage.java
+++ src/test/java/PageObjects/DevToMainPage.java
@@ -1,21 +1,21 @@
 package PageObjects;
-
-import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 public class DevToMainPage {
-    public String url = "https;//dev.to";
-    WebDriver driver;
-    @Before //sekcja before, a właściwie metoda setup, wykona się przed każdym testem
-    public void setup(){
-        System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe"); //1.wskazanie chrome.driver
-        driver = new ChromeDriver(); //przypisanie do pustej przeglądarki, przeglądarki chrome do wykonywania testów
-        driver.manage().window().maximize();
-        driver.get("https://dev.to");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //przed każdym kolejnym findElement/s poczekaj 10 sekund, zanim wywalisz error, co sekundę sprawdzaj czy element jest już dostępny
+    String url = "https://dev.to";
+    WebDriver driverInDevToMainPage;
+    @FindBy(xpath = "//a[@href='/pod']")
+    WebElement podcastsBtn;
+    public DevToMainPage(WebDriver driverFromPageObjectTests) {
+        this.driverInDevToMainPage = driverFromPageObjectTests;
+        //przypisanie zależności przeglądarki z klasy PageObjectTests do przelgądarki używanej w kalsie DevToMainPage
+        driverInDevToMainPage.get(url); //otwiermy stronę w przeglądarce
+        PageFactory.initElements(driverInDevToMainPage,this);
+    }
+    public DevToPodcastsPage selectPodcasts(){
+        podcastsBtn.click();
+        return new DevToPodcastsPage(driverInDevToMainPage);
     }
 }
